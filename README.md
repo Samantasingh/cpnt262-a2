01ab9516cfd0d93894d6e0f8e76dada76588eb81
Samanta S <samantalsingh@gmail.com>
2022-10-28 20:43:15 -0600

    fixed images

--- README.md
+++ README.md
@@ -3,15 +3,16 @@ If This, Then That
 Samanta Singh
 
 # GH Pages
-- [Live](https://samantasingh.github.io/cpnt262-a2/)
+[Live](https://samantasingh.github.io/cpnt262-a2/)
 
 # Attributions
- W3schools
- - [Style background property](https://www.w3schools.com/JSREF/prop_style_background.asp)
+
+[Style background property](https://www.w3schools.com/JSREF/prop_style_background.asp)
+[KodeBase](https://youtu.be/E6J2fosujWQ)
 
 Pexels
-- [Strawberry](https://www.pexels.com/photo/70746/)
-- [Green Apple](https://www.pexels.com/photo/693794/)
-- [Kiwi](https://www.pexels.com/photo/867349/)
-- [Blueberry](https://www.pexels.com/photo/1153655/)
+[Strawberry](https://www.pexels.com/photo/70746/)
+[Green Apple](https://www.pexels.com/photo/693794/)
+[Kiwi](https://www.pexels.com/photo/867349/)
+[Blueberry](https://www.pexels.com/photo/1153655/)
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,29 @@
-  // TODO: Selecting 'Make a selection' leadiing back to a gray background
-
-
   const backgroundImage = document.querySelector ('select')
   document.getElementById("backgroundImage").onchange = function() {
 
   let getValue = document.getElementById("backgroundImage");
   let value = getValue.value;
-  
+  // TODO: Selecting 'Make a selection' leadiing back to a gray background
     if (value == "strawberry") {
-      document.body.style.backgroundImage = ("url('/assets/images/strawberries.jpg')");
+      document.body.style.backgroundImage = ("url('assets/images/strawberries.jpg')");
       document.body.style.backgroundRepeat = ("no-repeat");
       document.body.style.backgroundSize = ("110rem");
       document.body.style.backgroundPosition = ("center");
   } 
     else if(value == "kiwi") {
-      document.body.style.backgroundImage = ("url('/assets/images/kiwi.jpg')");
+      document.body.style.backgroundImage = ("url('assets/images/kiwi.jpg')");
       document.body.style.backgroundRepeat = ("no-repeat");
       document.body.style.backgroundSize = ("110rem");
       document.body.style.backgroundPosition = ("center");
   }
     else if(value == "greenApple") {
-      document.body.style.backgroundImage = ("url('/assets/images/green-apples.jpg')");
+      document.body.style.backgroundImage = ("url('assets/images/green-apples.jpg')");
       document.body.style.backgroundRepeat = ("no-repeat");
       document.body.style.backgroundSize = ("110rem");
       document.body.style.backgroundPosition = ("center");
   }
     else if(value == "blueberry") {
-      document.body.style.backgroundImage = ("url('/assets/images/blueberries.jpg')");
+      document.body.style.backgroundImage = ("url('assets/images/blueberries.jpg')");
       document.body.style.backgroundRepeat = ("no-repeat");
       document.body.style.backgroundSize = ("110rem");
       document.body.style.backgroundPosition = ("center");
